d88c6024c8333751ac239fbafb2187a4abae462f
wojmo <wojmoc96@gmail.com>
2020-02-10 22:48:10 +0100

    tests

--- src/main/java/pl/math/uni/lodz/pl/pageObject/P10.java
+++ src/main/java/pl/math/uni/lodz/pl/pageObject/P10.java
@@ -1,5 +1,33 @@
 package pl.math.uni.lodz.pl.pageObject;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import pl.math.uni.lodz.pl.ChromeDriver;
+
 public class P10 {
 
+    private WebElement selectBtn;
+    private WebElement option;
+    private WebElement resultText;
+
+    public P10(){
+        selectBtn = ChromeDriver.getDriver()
+                .findElement(By.xpath("/html/body/div[2]/div/div[2]/div[1]/div[2]/select"));
+        option = ChromeDriver.getDriver()
+                .findElement(By.xpath("/html/body/div[2]/div/div[2]/div[1]/div[2]/select/option[4]"));
+        resultText = ChromeDriver.getDriver()
+                .findElement(By.xpath("/html/body/div[2]/div/div[2]/div[1]/div[2]/p[2]"));
+    }
+
+    public void selectBtnClick(){
+        selectBtn.click();
+    }
+
+    public void optionClick(){
+        option.click();
+    }
+
+    public String getResultText(){
+        return resultText.getText();
+    }
 }
--- src/test/java/pl/math/uni/lodz/pl/pageObject/Tests.java
+++ src/test/java/pl/math/uni/lodz/pl/pageObject/Tests.java
@@ -19,145 +19,145 @@ public class Tests {
         //ChromeDriver.quit();
     }
 
-//    @Test
-//    public void T1() {
-//        ChromeDriver.navigate(Page.getPage() + "basic-first-form-demo.html");
-//        P1 page = new P1();
-//        page.setInputTextElement("testMessage");
-//        page.clickSubmitButton();
-//        assertEquals("testMessage",page.getResultText());
-//    }
-//
-//    @Test
-//    public void T2() {
-//        ChromeDriver.navigate(Page.getPage() + "basic-first-form-demo.html");
-//        P2 page = new P2();
-//        page.setInputAField("1");
-//        page.setInputBField("2");
-//        ChromeDriver.await();
-//        page.clickSubmitButton();
-//        ChromeDriver.await();
-//        assertEquals("3",page.getResultText());
-//    }
-//
-//    @Test
-//    public void T3(){
-//        ChromeDriver.navigate(Page.getPage() + "basic-checkbox-demo.html");
-//        P3 page = new P3();
-//        page.setRbElement();
-//        assertEquals(page.getResultText(),"Success - Check box is checked");
-//        page.checkUncheckAllButtonClick();
-//        assertEquals(page.getCheckAllButtonText(),"Uncheck All");
-//        for ( WebElement el : page.getChkBoxes() ) {
-//            if ( !el.isSelected() ) {
-//                assertFalse(true);
-//            }
-//        }
-//        ChromeDriver.await();
-//        page.checkUncheckAllButtonClick();
-//        assertEquals(page.getCheckAllButtonText(),"Check All");
-//        for ( WebElement el : page.getChkBoxes() ) {
-//            if ( el.isSelected() ) {
-//                assertFalse(true);
-//            }
-//        }
-//    }
-//
-//    @Test
-//    public void T4(){
-//        ChromeDriver.navigate(Page.getPage() + "bootstrap-date-picker-demo.html");
-//        P4 page = new P4();
-//        page.inputDate(page.getNow());
-//        assertEquals(page.getInputValue(),page.getNow());
-//        ChromeDriver.await();
-//        page.inputDate(page.addDaysToToday(7));
-//        ChromeDriver.await();
-//        assertEquals(page.getInputValue(),page.addDaysToToday(7));
-//        ChromeDriver.await();
-//        page.btnTodayClick();
-//        assertEquals(page.getInputValue(),page.getNow());
-//    }
-//
-//    @Test
-//    public void T5() throws InterruptedException {
-//        ChromeDriver.navigate(Page.getPage() + "bootstrap-download-progress-demo.html");
-//        P5 page = new P5();
-//        page.BtnStartDownloadClick();
-//        assertTrue(page.isDownloaded());
-//    }
-//
-//    @Test
-//    public void T6(){
-//        ChromeDriver.navigate(Page.getPage() + "bootstrap-modal-demo.html");
-//        P6 page = new P6();
-//        page.singleModalButtonClick();
-//        ChromeDriver.await();
-//        try{
-//            page.singleModalCloseButtonClick();
-//        }
-//        catch(ElementNotInteractableException e){
-//            assertFalse(true);
-//        }
-//        ChromeDriver.await();
-//
-//        page.multipleModalButtonClick();
-//        ChromeDriver.await();
-//        try{
-//            page.multipleInnerModalButtonClick();
-//        }
-//        catch(ElementNotInteractableException e){
-//            assertFalse(true);
-//        }
-//
-//        ChromeDriver.await();
-//        try{
-//            page.multipleInnerModalCloseButtonClick();
-//        }
-//        catch(ElementNotInteractableException e){
-//            assertFalse(true);
-//        }
-//
-//        ChromeDriver.await();
-//        try{
-//            page.multipleModalButtonCloseClick();
-//        }
-//        catch(ElementNotInteractableException e){
-//            assertFalse(true);
-//        }
-//
-//    }
-//
-//    @Test
-//    public void T7(){
-//        ChromeDriver.navigate(Page.getPage() + "bootstrap-alert-messages-demo.html");
-//        P7 page = new P7();
-//        page.autoCloseSuccessMsgButtonClick();
-//        ChromeDriver.await();
-//        assertTrue(page.isSuccessMsgHidden());
-//    }
-//
-//    @Test
-//    public void T8(){
-//        ChromeDriver.navigate(Page.getPage() + "table-search-filter-demo.html");
-//        P8 page = new P8();
-//        page.setIdInputValue("1");
-//        assertEquals("1",page.getIdCellValue());
-//        page.setTaskInputValue("jQuery library");
-//        assertEquals("jQuery library",page.getTaskCellValue());
-//        page.setAssigneeInputValue("Mike");
-//        assertEquals("Mike Trout",page.getAssigneeCellValue());
-//        page.setStatusInputValue("in progress");
-//        assertEquals("in progress",page.getStatusCellValue());
-//        page.filterButtonClick();
-//        page.setFilterIdInputValue("2");
-//        assertEquals("2",page.getIdFilterCell());
-//        page.setFilterUsernameInputValue("mar");
-//        assertEquals("markino",page.getUsernameFilterCell());
-//        page.setFilterFirstNameInputValue("Daniel");
-//        assertEquals("Daniel",page.getFirstNameFilterCell());
-//        page.setFilterLastNameInputValue("Dima");
-//        assertEquals("Dimarison",page.getFilterLastNameCell());
-//    }
+    @Test
+    public void T1() {
+        ChromeDriver.navigate(Page.getPage() + "basic-first-form-demo.html");
+        P1 page = new P1();
+        page.setInputTextElement("testMessage");
+        page.clickSubmitButton();
+        assertEquals("testMessage",page.getResultText());
+    }
+
+    @Test
+    public void T2() {
+        ChromeDriver.navigate(Page.getPage() + "basic-first-form-demo.html");
+        P2 page = new P2();
+        page.setInputAField("1");
+        page.setInputBField("2");
+        ChromeDriver.await();
+        page.clickSubmitButton();
+        ChromeDriver.await();
+        assertEquals("3",page.getResultText());
+    }
+
+    @Test
+    public void T3(){
+        ChromeDriver.navigate(Page.getPage() + "basic-checkbox-demo.html");
+        P3 page = new P3();
+        page.setRbElement();
+        assertEquals(page.getResultText(),"Success - Check box is checked");
+        page.checkUncheckAllButtonClick();
+        assertEquals(page.getCheckAllButtonText(),"Uncheck All");
+        for ( WebElement el : page.getChkBoxes() ) {
+            if ( !el.isSelected() ) {
+                assertFalse(true);
+            }
+        }
+        ChromeDriver.await();
+        page.checkUncheckAllButtonClick();
+        assertEquals(page.getCheckAllButtonText(),"Check All");
+        for ( WebElement el : page.getChkBoxes() ) {
+            if ( el.isSelected() ) {
+                assertFalse(true);
+            }
+        }
+    }
+
+    @Test
+    public void T4(){
+        ChromeDriver.navigate(Page.getPage() + "bootstrap-date-picker-demo.html");
+        P4 page = new P4();
+        page.inputDate(page.getNow());
+        assertEquals(page.getInputValue(),page.getNow());
+        ChromeDriver.await();
+        page.inputDate(page.addDaysToToday(7));
+        ChromeDriver.await();
+        assertEquals(page.getInputValue(),page.addDaysToToday(7));
+        ChromeDriver.await();
+        page.btnTodayClick();
+        assertEquals(page.getInputValue(),page.getNow());
+    }
+
+    @Test
+    public void T5() throws InterruptedException {
+        ChromeDriver.navigate(Page.getPage() + "bootstrap-download-progress-demo.html");
+        P5 page = new P5();
+        page.BtnStartDownloadClick();
+        assertTrue(page.isDownloaded());
+    }
+
+    @Test
+    public void T6(){
+        ChromeDriver.navigate(Page.getPage() + "bootstrap-modal-demo.html");
+        P6 page = new P6();
+        page.singleModalButtonClick();
+        ChromeDriver.await();
+        try{
+            page.singleModalCloseButtonClick();
+        }
+        catch(ElementNotInteractableException e){
+            assertFalse(true);
+        }
+        ChromeDriver.await();
+
+        page.multipleModalButtonClick();
+        ChromeDriver.await();
+        try{
+            page.multipleInnerModalButtonClick();
+        }
+        catch(ElementNotInteractableException e){
+            assertFalse(true);
+        }
+
+        ChromeDriver.await();
+        try{
+            page.multipleInnerModalCloseButtonClick();
+        }
+        catch(ElementNotInteractableException e){
+            assertFalse(true);
+        }
+
+        ChromeDriver.await();
+        try{
+            page.multipleModalButtonCloseClick();
+        }
+        catch(ElementNotInteractableException e){
+            assertFalse(true);
+        }
+
+    }
+
+    @Test
+    public void T7(){
+        ChromeDriver.navigate(Page.getPage() + "bootstrap-alert-messages-demo.html");
+        P7 page = new P7();
+        page.autoCloseSuccessMsgButtonClick();
+        ChromeDriver.await();
+        assertTrue(page.isSuccessMsgHidden());
+    }
+
+    @Test
+    public void T8(){
+        ChromeDriver.navigate(Page.getPage() + "table-search-filter-demo.html");
+        P8 page = new P8();
+        page.setIdInputValue("1");
+        assertEquals("1",page.getIdCellValue());
+        page.setTaskInputValue("jQuery library");
+        assertEquals("jQuery library",page.getTaskCellValue());
+        page.setAssigneeInputValue("Mike");
+        assertEquals("Mike Trout",page.getAssigneeCellValue());
+        page.setStatusInputValue("in progress");
+        assertEquals("in progress",page.getStatusCellValue());
+        page.filterButtonClick();
+        page.setFilterIdInputValue("2");
+        assertEquals("2",page.getIdFilterCell());
+        page.setFilterUsernameInputValue("mar");
+        assertEquals("markino",page.getUsernameFilterCell());
+        page.setFilterFirstNameInputValue("Daniel");
+        assertEquals("Daniel",page.getFirstNameFilterCell());
+        page.setFilterLastNameInputValue("Dima");
+        assertEquals("Dimarison",page.getFilterLastNameCell());
+    }
 
     @Test
     public void T9(){
@@ -171,7 +171,11 @@ public class Tests {
 
     @Test
     public void T10(){
-        
+        ChromeDriver.navigate(Page.getPage() + "basic-select-dropdown-demo.html");
+        P10 page = new P10();
+        page.selectBtnClick();
+        page.optionClick();
+        assertEquals("Day selected :- Tuesday", page.getResultText());
     }
 
 }
